3ac1a8513c6673b72b005c20f6ea37b385673a63
Carnacky79 <f.lombardi79@gmail.com>
2024-08-11 07:09:03 +0200

    Added Breadrcrumbs on anagrafica.php

--- anagrafica.php
+++ anagrafica.php
@@ -95,6 +95,7 @@
             <div class="container-fluid px-4">
                 <h1 class="mt-4">Gestione Clienti</h1>
                 <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item"><a href="index.php">Principale</a></li>
                     <li class="breadcrumb-item active">Gestione Clienti</li>
                 </ol>
 
