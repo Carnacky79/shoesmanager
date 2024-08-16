6b54c71b329c94727bb6fde0e809e39437281aa6
Carnacky79 <f.lombardi79@gmail.com>
2024-08-16 23:32:49 +0200

    Lavor lowercase

--- app/db/labelsQueries.php
+++ app/db/labelsQueries.php
@@ -5,18 +5,18 @@ global $conn;
 
 function CountLavoriTot($conn)
 {
-    $sql = 'SELECT COUNT(*) as totale FROM Lavori WHERE data_fine IS NULL';
+    $sql = 'SELECT COUNT(*) as totale FROM lavori WHERE data_fine IS NULL';
     return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
 }
 
 function CountLavoriSenzaAttributi($conn)
 {
-    $sql = 'SELECT COUNT(*) as totale FROM Lavori WHERE data_fine IS NULL AND attributo_id = 0';
+    $sql = 'SELECT COUNT(*) as totale FROM lavori WHERE data_fine IS NULL AND attributo_id = 0';
     return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
 }
 
 function CountLavoriAttributi($conn, $id_attributo){
-    $sql = 'SELECT COUNT(*) FROM Lavori WHERE data_fine IS NULL AND attributo_id = ?';
+    $sql = 'SELECT COUNT(*) FROM lavori WHERE data_fine IS NULL AND attributo_id = ?';
     $prepared = $conn->prepare($sql);
     $prepared->bind_param('i', $id_attributo);
     $prepared->execute();
