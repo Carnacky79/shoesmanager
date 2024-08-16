e3ef0abdd55ad897f04d521ab5fab0bb821e02df
mrpit1986 <mrpit1986@hotmail.com>
2024-08-16 21:59:00 +0200

    Modifiche per colori riga lavori chiusi

--- app/db/labelsQueries.php
+++ app/db/labelsQueries.php
@@ -3,4 +3,25 @@
 require_once 'dbConnection.php';
 global $conn;
 
+function CountLavoriTot($conn)
+{
+    $sql = 'SELECT COUNT(*) FROM Lavori WHERE DataFine IS NULL';
+    return $conn->query($sql);
+}
+
+function CountLavoriSenzaAttributi($conn)
+{
+    $sql = 'SELECT COUNT(*) FROM Lavori WHERE DataFine IS NULL AND attributi = 0';
+    return $conn->query($sql);
+}
+
+function CountLavoriAttributi($conn)
+{
+    $sql = 'SELECT COUNT(*) FROM Lavori WHERE DataFine IS NULL AND attributi = 0';
+    return $conn->query($sql);
+}
+
+
+
+
 
