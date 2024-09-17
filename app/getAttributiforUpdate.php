fb87c669ab3048e4d03f57874a8d89dcbf1b786c
mrpit1986 <mrpit1986@hotmail.com>
2024-09-17 19:05:34 +0200

    Attributo 0 Hide
    Hide Attributo 0

--- app/getAttributiforUpdate.php
+++ app/getAttributiforUpdate.php
@@ -3,5 +3,5 @@
 require_once 'db/dbConnection.php';
 global $conn;
 
-$sql = 'SELECT * FROM attributi';
+$sql = 'SELECT * FROM attributi WHERE id > 0';
 $attributi = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
